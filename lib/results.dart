a15cde3b58e010ecdc1edad2e016dcbdfdd987ea
Sharon Rodrigues <sr979151@gmail.com>
2023-09-26 00:00:02 +0530

    finished , more enhancements

--- lib/results.dart
+++ lib/results.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:percent_indicator/circular_percent_indicator.dart';
+import 'package:quiz/quizpage.dart';
 
 class Results extends StatelessWidget {
   const Results({super.key, required this.points});
@@ -9,23 +10,34 @@ class Results extends StatelessWidget {
     print(points);
     return Scaffold(
       appBar: AppBar(title: Text("Results")),
-      body: Center(
-          child: CircularPercentIndicator(
-        animation: true,
-        radius: 60,
-        percent: points / 10,
-        progressColor: points > 5 && points < 8
-            ? Colors.blue
-            : points < 5
-                ? Colors.red
-                : Colors.amber,
-        widgetIndicator: Text("${points * 10}"),
-        center: Text(
-          "${points * 10}",
-          style: TextStyle(
-              fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
-        ),
-      )),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Center(
+              child: CircularPercentIndicator(
+            animation: true,
+            radius: 60,
+            percent: points / 10,
+            progressColor: points > 5 && points < 8
+                ? Colors.blue
+                : points < 5
+                    ? Colors.red
+                    : Colors.amber,
+            widgetIndicator: Text("${points * 10}"),
+            center: Text(
+              "${points * 10}",
+              style: TextStyle(
+                  fontSize: 30, fontWeight: FontWeight.bold, color: Colors.red),
+            ),
+          )),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.of(context).pushReplacement(
+                    MaterialPageRoute(builder: (context) => Quizpage()));
+              },
+              child: points < 5 ? Text("Try Again") : Text("Good Job"))
+        ],
+      ),
     );
   }
 }
